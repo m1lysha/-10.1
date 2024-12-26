f2ff1f7f5da29eff928446efcea12c6dfaa52fee
RobotComp.ru <RobotComp.ru@DESKTOP-94TEC7C>
2024-12-26 19:30:59 +0300

    Добавьте файлы проекта.

---
+++ лаб10.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Book
+{
+    protected string title;
+    protected int pageCount;
+    protected double price;
+    public Book(string title, int pageCount, double price)
+    {
+        this.title = title;
+        this.pageCount = pageCount;
+        this.price = price;
+    }
+    public virtual string GetInfo()
+    {
+        return $"Title: {title}, Pages: {pageCount}, Price: {price:C}";
+    }
+}
+
+class LibraryBook : Book
+{
+    private double discountPercentage;
+    public LibraryBook(string title, int pageCount, double price, double discountPercentage)
+        : base(title, pageCount, price)
+    {
+        this.discountPercentage = discountPercentage;
+    }
+    public double CalculateDiscountedPrice()
+    {
+        return price - (price * discountPercentage / 100);
+    }
+    public override string GetInfo()
+    {
+        return $"{base.GetInfo()}, Discount: {discountPercentage}%, Discounted Price: {CalculateDiscountedPrice():C}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LibraryBook libraryBook = new LibraryBook("C# Programming", 300, 29.99, 10);
+        Console.WriteLine(libraryBook.GetInfo());
+        double discountedPrice = libraryBook.CalculateDiscountedPrice();
+        Console.WriteLine($"Discounted Price: {discountedPrice:C}");
+    }
+}
